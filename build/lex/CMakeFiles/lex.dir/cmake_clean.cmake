9f0482060db5bba548af1bcb5a04c4ff0b238892
xiuq04 <48477489+xiuq04@users.noreply.github.com>
2022-06-17 12:46:00 +0800

    Add files via upload

---
+++ build/lex/CMakeFiles/lex.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/lex.dir/pl0_lex.c.o"
+  "liblex.pdb"
+  "liblex.a"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/lex.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ build/syntax/CMakeFiles/tax.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/tax.dir/pl0_tax.c.o"
+  "libtax.pdb"
+  "libtax.a"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/tax.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
